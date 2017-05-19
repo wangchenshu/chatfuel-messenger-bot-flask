a97861a621a91d9e369ca84a10b74b7f401c1bc3
Walter Wang <csw.dawm@gmail.com>
2017-05-19 21:48:01 +0800

    rename api to app and add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:latest
+MAINTAINER Rajdeep Dua "dua_rajdeep@yahoo.com"
+RUN apt-get update -y
+RUN apt-get install -y python-pip python-dev build-essential
+RUN apt-get install -y locales
+RUN locale-gen en_US.UTF-8  
+ENV LANG en_US.UTF-8  
+ENV LANGUAGE en_US:en  
+ENV LC_ALL en_US.UTF-8
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
+ENTRYPOINT ["python"]
+CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-from dokuwiki import DokuWiki, DokuWikiError
-from datetime import datetime
 
 import message
 
 app = Flask(__name__)
 
 @app.route("/register", methods=['POST'])
-def register():
-    now = datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    url_str = 'http://new.hackingthursday.org/dokuwiki/%d%02d%02d' % (year, month, day)
-    
-    '''
-    try:
-        wiki = DokuWiki(url_str, 'USER', 'PASSWORD')
-    except (DokuWikiError, Exception) as err:
-        print('unable to connect: %s' % err
-    '''
-
+def register():    
     chatfuel_user_id = request.form['chatfuel user id']
     first_name = request.form['first name']
     last_name = request.form['last name']
